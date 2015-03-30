58e6cf26229d31cf4cf3c88b903fae3e01c94d7f
Damon Oehlman <damon.oehlman@gmail.com>
2015-03-30 11:58:56 +1100

    Ensure we find a node_modules directory for requires

--- index.sh
+++ index.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
 require() {
   local MODULE_PATH=$(dirname "$(readlink -f ${BASH_SOURCE[1]})")
-  local TARGET_PATH=$MODULE_PATH/node_modules/$1
+  local TARGET_PATH
+
+  # while we don't have a node_modules path in our local path, dive up a dir
+  while [ ! -d $MODULE_PATH/node_modules ]; do
+    MODULE_PATH=$(dirname $MODULE_PATH)
+    if [ $MODULE_PATH == '/' ]; then
+      echo "could not locate module path";
+      exit 2;
+    fi
+  done
+
+  TARGET_PATH=$MODULE_PATH/node_modules/$1
 
   # if not in the specified directory, check if we have a bash-$1 directory
   if [ ! -d $TARGET_PATH ]; then
